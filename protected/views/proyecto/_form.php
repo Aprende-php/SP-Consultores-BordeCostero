82d0787e0f2fbf48793a8bf15f066ba1850e5507
Ruben <rbntejeda@hotmail.com>
2014-05-25 16:30:07 -0400

    Actaulizacion de PRoyectoi

--- protected/views/proyecto/_form.php
+++ protected/views/proyecto/_form.php
@@ -18,21 +18,13 @@
     <?php echo $form->errorSummary($model); ?>
     <?php echo $form->textFieldControlGroup(Persona::model()->findByPk($model->PER_CORREL), 'PER_RUT', array('disabled' => true));?>
     <?php echo $form->dropDownListControlGroup($model,'INT_CORREL',CHtml::listData(Institucion::model()->findAll(),'INT_CORREL','INT_NOMBRE'), array('empty' => 'Escoja una Institución')); ?>
-     <?php  
-     $this->widget('CJuiDateTimePicker',array(
-                'language'=>'',
-                'model'=>$model,                                // Model object
-                'attribute'=>'start_date_time', // Attribute name
-                'mode'=>'datetime',                     // Use "time","date" or "datetime" (default)
-                'options'=>array(),                     // jquery plugin options
-                'htmlOptions'=>array('readonly'=>true) // HTML options
-        ));                    
-     ?>
+    <label>Fecha del Proyecto</label>
     <?php echo $form->dateField($model,'PRO_FECHA',array('min'=>'1990-01-01','max'=>date('Y-m-d'))); ?>
+    <br>
     <?php echo $form->textAreaControlGroup($model,'PRO_NOMBRE',array('rows'=>1,'placeholder'=>'Agregue el Nombre del Proyecto')); ?>
     <?php echo $form->textAreaControlGroup($model,'PRO_DESCRIPCION',array('rows'=>6,'placeholder'=>'Agregue una breve descripción')); ?>
     <?php echo $form->textFieldControlGroup($model,'PRO_MONTO',array('maxlength'=>12,'min'=>0,'required'=>'')); ?>
-<br>
+    <br>
     <?php echo BsHtml::submitButton('Submit', array('color' => BsHtml::BUTTON_COLOR_PRIMARY)); ?>
 
 <?php $this->endWidget(); ?>
